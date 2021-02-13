57ef0a4a38df65cc389067707b57176d9fd67b24
Candilola <68983496+Candilola@users.noreply.github.com>
2021-02-13 10:57:27 +1030

    burger js

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+// function fade(){
+//     $(".preloader").fadeOut("slow");
+// }
+// setTimeout(fadeOut, 2000);
+console.log("test");
+
+// setTimeout(function(){ alert("Hello"); }, 3000);
